5fe6f4f9ed742e54eeef043fc71f653ba31361f2
Peter Laudy <peter@dot42.com>
2014-03-11 09:57:49 +0100

    Changed versions to 1.0.1.80 because of the latest release.

--- Generated/v3.0/Version.cs
+++ Generated/v3.0/Version.cs
@@ -14,8 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 [assembly: System.Reflection.AssemblyVersion("3.0.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("3.0.0.79")]
-[assembly: System.Reflection.AssemblyInformationalVersion("3.0, Dot42 1.0.1.79")]
+[assembly: System.Reflection.AssemblyFileVersion("3.0.0.80")]
+[assembly: System.Reflection.AssemblyInformationalVersion("3.0, Dot42 1.0.1.80")]
 #if !BASELIB
 [assembly: System.Runtime.Versioning.TargetFramework("Dot42,Version=v3.0", FrameworkDisplayName = "Dot42")]
 #endif
