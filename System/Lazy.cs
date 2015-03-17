dc9999466f6a835829afe5a811c460ec350e2741
Olaf Pettersson <olaf.pettersson@web.de>
2015-03-17 02:43:26 +0100

    added Lazy, SemaphoreSlim and TimeoutException

---
+++ System/Lazy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Java.Util.Concurrent.Atomic;
+
+namespace System
+{
+    public class Lazy<T> where T: class 
+    {
+        private readonly Func<T> _initializor;
+        private readonly AtomicReference<T> _instance = new AtomicReference<T>();
+
+        public T Value
+        {
+            get
+            {
+                T foo = _instance.Get();
+                if (foo != null) return foo;
+                _instance.CompareAndSet(null, _initializor());
+                return _instance.Get();
+            }
+        }
+
+        public Lazy(Func<T> initializor)
+        {
+            _initializor = initializor;
+        }
+    }
+}
---
+++ System/Threading/SemaphoreSlim.cs
@@ -0,0 +1,94 @@
+﻿using System.Threading.Tasks;
+using Java.Util.Concurrent;
+
+namespace System.Threading
+{
+    public class SemaphoreSlim /*: IDisposable*/
+    {
+        private readonly Semaphore _sem;
+
+        public SemaphoreSlim(int initialCount)
+        {
+            _sem = new Semaphore(initialCount);
+        }
+
+
+        //public void Dispose()
+        //{
+        //    sem.
+        //}
+
+        public Task WaitAsync(CancellationToken token)
+        {
+            return Task.Run(() => Wait(token), token);
+        }
+
+        public Task WaitAsync(TimeSpan duration, CancellationToken token)
+        {
+            return Task.Run(() => Wait(duration, token), token);
+        }
+
+        public Task WaitAsync(Int32 duration, CancellationToken token)
+        {
+            return Task.Run(() => Wait(duration, token), token);
+        }
+
+        public Task WaitAsync()
+        {
+            return Task.Run(Wait);
+        }
+
+        public void Wait()
+        {
+            _sem.AcquireUninterruptibly();
+        }
+
+        public void Wait(CancellationToken token)
+        {
+            var thread = Thread.GetCurrentThread();
+            try
+            {
+                using (token.Register(thread.Interrupt))
+                    _sem.Acquire();            
+            }
+            catch (Java.Lang.InterruptedException ex)
+            {
+                throw new OperationCanceledException(ex.Message, ex, token);
+            }
+        }
+
+        public bool Wait(Int32 milliseconds,  CancellationToken token)
+        {
+            var thread = Thread.GetCurrentThread();
+            try
+            {
+                using (token.Register(thread.Interrupt))
+                    return _sem.TryAcquire(milliseconds, TimeUnit.MILLISECONDS);
+            }
+            catch (Java.Lang.InterruptedException ex)
+            {
+                throw new OperationCanceledException("operation canceled", ex, token);
+            }
+        }
+
+        public bool Wait(TimeSpan timeSpan, CancellationToken token)
+        {
+            return Wait((Int32)timeSpan.TotalMilliseconds, token);
+        }
+
+        public bool Wait(Int32 milliseconds)
+        {
+            return Wait(milliseconds, default(CancellationToken));
+        }
+
+        public void Release(int count)
+        {
+            _sem.Release(count);
+        }
+
+        public void Release()
+        {
+            _sem.Release();
+        }
+    }
+}
