717375dc3748fbe14eaef982e5b30d2e8ea65ad9
Olaf Pettersson <olaf.pettersson@web.de>
2015-04-19 17:46:20 +0200

    add SerializationMethodAttribute to allow the compiler to not prune serialized types

---
+++ Dot42/SerializationMethodAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dot42
+{
+    /// <summary>
+    /// Specifies this method as a serialization method. Arguments passed into this method,
+    /// including generic arguments, and the return value will have all their fields 
+    /// and properties included in the final output.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    [Ignore]
+    public class SerializationMethodAttribute : Attribute
+    {
+    }
+}
