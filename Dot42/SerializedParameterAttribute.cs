97a5a1780910bd5e6e69af4346b0c374513c10cc
Olaf Pettersson <olaf.pettersson@web.de>
2015-05-23 08:49:58 +0200

    fix quickhelp on SerializedParameterAttribute

--- Dot42/SerializedParameterAttribute.cs
+++ Dot42/SerializedParameterAttribute.cs
@@ -5,10 +5,13 @@ namespace Dot42
     /// <summary>
     /// Specifies this parameter is used in serialization. Types and objects passed
     /// as this parameter will have all their public fields and public and private 
-    /// properties preserved and not pruned.
+    /// properties preserved and not pruned. 
     /// <para/>
     /// <para/>
     /// Can be used on normal and generic method parameters.
+    /// <para>
+    /// Propagates recursivly to the types of fields and properties of the passed type.
+    /// </para>
     /// <para/>
     /// Note that this Attribute is not inherited when used on generic method parameters.
     /// </summary>
